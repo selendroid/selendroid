1476cb2b749297947ba3c9f2d6b11fba5470333e
Dary, Dominik <ddary@ebay.com>
2013-02-15 16:42:36 +0100

    added more webview tests

--- selendroid-large-tests/src/main/java/org/openqa/selendroid/webviewdrivertests/ElementFindingTest.java
+++ selendroid-large-tests/src/main/java/org/openqa/selendroid/webviewdrivertests/ElementFindingTest.java
@@ -13,9 +13,13 @@
  */
 package org.openqa.selendroid.webviewdrivertests;
 
+import static org.openqa.selendroid.webviewdrivertests.waiter.TestWaiter.waitFor;
+import static org.openqa.selendroid.webviewdrivertests.waiter.WaitingConditions.pageTitleToBe;
+
 import org.openqa.selendroid.tests.internal.BaseAndroidTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -39,14 +43,70 @@ public class ElementFindingTest extends BaseAndroidTest {
     driver.findElement(By.linkText(page)).click();
     driver.switchTo().window("WEBVIEW");
   }
-
-  @Test
-  public void testShouldNotBeAbleToLocateASingleElementThatDoesNotExist() {
+  
+  protected void openWebdriverTestPageByOpeningWebview(String page) {
     openWebView();
-    openWebdriverTestPage(HtmlTestData.FORM_PAGE);
+    openWebdriverTestPage(page);
+  }
+
+  @Test()
+  public void testShouldNotBeAbleToLocateASingleElementThatDoesNotExist() {
+    openWebdriverTestPageByOpeningWebview(HtmlTestData.FORM_PAGE);
 
     try {
       driver.findElement(By.id("nonExistantButton"));
+      Assert.fail("Should not have succeeded");
+    } catch (NoSuchElementException e) {
+      // this is expected
+    }
+  }
+
+  @Test
+  public void testShouldBeAbleToClickOnLinkIdentifiedByText() {
+    openWebdriverTestPageByOpeningWebview(HtmlTestData.XHTML_TEST_PAGE);
+    driver.findElement(By.linkText("click me")).click();
+
+    waitFor(pageTitleToBe(driver, "We Arrive Here"));
+    Assert.assertEquals(driver.getTitle(), "We Arrive Here");
+  }
+
+  @Test
+  public void testshouldBeAbleToClickOnLinkIdentifiedById() {
+    openWebdriverTestPageByOpeningWebview(HtmlTestData.XHTML_TEST_PAGE);
+    driver.findElement(By.id("linkId")).click();
+
+    waitFor(pageTitleToBe(driver, "We Arrive Here"));
+
+    Assert.assertEquals(driver.getTitle(), "We Arrive Here");
+  }
+
+  @Test
+  public void testShouldThrowAnExceptionWhenThereIsNoLinkToClickAndItIsFoundWithLinkText() {
+    openWebdriverTestPageByOpeningWebview(HtmlTestData.XHTML_TEST_PAGE);
+
+    try {
+      driver.findElement(By.linkText("Not here either"));
+      Assert.fail("Should not have succeeded");
+    } catch (NoSuchElementException e) {
+      // this is expected
+    }
+  }
+
+  @Test
+  public void testShouldfindAnElementBasedOnId() {
+    openWebdriverTestPageByOpeningWebview(HtmlTestData.FORM_PAGE);
+
+    WebElement element = driver.findElement(By.id("checky"));
+
+    Assert.assertEquals(element.isSelected(), false);
+  }
+
+  @Test
+  public void testShouldNotBeAbleTofindElementsBasedOnIdIfTheElementIsNotThere() {
+    openWebdriverTestPageByOpeningWebview(HtmlTestData.FORM_PAGE);
+
+    try {
+      driver.findElement(By.id("notThere"));
       Assert.fail("Should not have succeeded");
     } catch (NoSuchElementException e) {
       // this is expected
