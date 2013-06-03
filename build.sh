4dc5e24339acfaf72c4e9950082ba19422e67a10
Dominik Dary <ddary@acm.org>
2013-06-03 15:13:08 +0200

    cleanup

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# script to simplify build and deploy
-mvn clean install
-adb install -r selendroid-server/target/selendroid-server-0.4-SNAPSHOT.apk
-./selendroid-server/start-server.sh 
--- selendroid-server/start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'Starting Selendroid-server'
-adb shell am instrument -e main_activity 'io.selendroid.testapp.HomeScreenActivity' io.selendroid/.ServerInstrumentation
-
-echo 'Activating port forwarding to port 8080'
-# activate port forwarding (local port : device port )
- adb forward tcp:8080 tcp:8080
