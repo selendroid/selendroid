87c510566971d222446fe0ae2dd49bd386ba4fdf
Dominik Dary <ddary@acm.org>
2013-07-18 11:27:59 +0200

    Update user_registration_test.rb

--- selendroid-test-app/src/test/ruby/user_registration_test.rb
+++ selendroid-test-app/src/test/ruby/user_registration_test.rb
@@ -60,14 +60,21 @@ class ExampleTest < Test::Unit::TestCase
     caps.version = "5"
     caps.platform = :linux
     caps.proxy = nil
-    caps[:maxInstances]="1"
+    caps[:aut] = "io.selendroid.testapp:0.5.0-SNAPSHOT"
+    caps[:locale]="de_DE"
+    caps[:browserName]="selendroid"
+
+    @driver = Selenium::WebDriver.for(
+    :remote,
+    :url => "http://localhost:5555/wd/hub",
+    :desired_capabilities => caps)
     
     client = Selenium::WebDriver::Remote::Http::Default.new
     client.timeout = 120 # seconds
     
     @driver = Selenium::WebDriver.for(
     :remote, :http_client => client, 
-    :url => "http://localhost:8090/wd/hub",
+    :url => "http://localhost:5555/wd/hub",
     :desired_capabilities => caps)
   end
 
