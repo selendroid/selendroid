3c1913d7bec9babc1d1c17ae5f5c35587fa6ce7d
Dominik Dary <ddary@acm.org>
2013-12-25 10:58:13 +0100

    Fixing handling of url loading.

--- android-driver/src/main/java/io/selendroid/androiddriver/WebViewActivity.java
+++ android-driver/src/main/java/io/selendroid/androiddriver/WebViewActivity.java
@@ -1,20 +1,40 @@
+/*
+ * Copyright 2012-2013 eBay Software Foundation and selendroid committers.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.selendroid.androiddriver;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_web_view);
-		WebView webview = (WebView) findViewById(R.id.webview);
-		webview.loadData(
-				"<html><body>" +
-			    "<h1 id='AndroidDriver'>Android Driver</h1>" +
-				"</body></html>", 
-				"text/html", "UTF-8");
-	}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_web_view);
+    WebView webview = (WebView) findViewById(R.id.webview);
+    webview.setWebViewClient(new AndroidDriverClient());
+    webview.loadData("<html><body>" + "<h1 id='AndroidDriver'>Android Driver</h1>"
+        + "</body></html>", "text/html", "UTF-8");
+  }
+
+  private class AndroidDriverClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+      view.loadUrl(url);
+      return true;
+    }
+  }
 }
