8afc6b41dbdafd40b2b47d569e3ae918b4ae7ca7
Juyuan Yang <juyuanyang@fb.com>
2014-09-22 15:45:58 -0700

    Make the class simpler, based on @mkonicek's comment

--- selendroid-server/src/main/java/io/selendroid/LightweightInstrumentation.java
+++ selendroid-server/src/main/java/io/selendroid/LightweightInstrumentation.java
@@ -13,81 +13,41 @@
  */
 package io.selendroid;
 
-import android.app.Activity;
-import android.app.Application;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.Handler;
-import io.selendroid.android.ActivitiesReporter;
 import io.selendroid.extension.ExtensionLoader;
-import io.selendroid.server.ServerDetails;
 import io.selendroid.server.model.ExternalStorage;
 import io.selendroid.util.SelendroidLogger;
-import org.json.JSONArray;
-
-import java.util.Set;
 
 /**
  * A light weight instrumentation without server
  */
-public class LightweightInstrumentation extends Instrumentation implements ServerDetails {
-    private ActivitiesReporter activitiesReporter = new ActivitiesReporter();
-    private static LightweightInstrumentation instance = null;
-    public static String mainActivityName = null;
-
+public class LightweightInstrumentation extends Instrumentation {
     /**
      * Arguments this instrumentation was started with.
      */
     public InstrumentationArguments args = null;
     private ExtensionLoader extensionLoader;
 
-    public void startMainActivity() {
-        finishAllActivities();
-        startActivity(mainActivityName);
-    }
-
-    public void startActivity(String activityClassName) {
-        finishAllActivities();
-
-        // Start the new activity
-        Intent intent = new Intent();
-        intent.setClassName(getTargetContext(), activityClassName);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.setAction(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        getTargetContext().startActivity(intent);
-    }
-
-    public void finishAllActivities() {
-        Set<Activity> activities = getActivities();
-        if (activities != null && !activities.isEmpty()) {
-            for (Activity activity : activities) {
-                activity.finish();
-            }
-        }
-    }
-
     @SuppressWarnings("unchecked")
     @Override
     public void onCreate(Bundle arguments) {
+        SelendroidLogger.info("Light weight Instrumentation initialized.");
+        final String mainActivityName = arguments.getString("main_activity");
+        final LightweightInstrumentation instance = this;
+
         Handler handler = new Handler();
         this.args = new InstrumentationArguments(arguments);
-
-        mainActivityName = arguments.getString("main_activity");
-
-        SelendroidLogger.info("Instrumentation initialized with main activity: " + mainActivityName);
-        instance = this;
-
         final Context context = getTargetContext();
+
         if (args.isLoadExtensions()) {
             extensionLoader = new ExtensionLoader(context, ExternalStorage.getExtensionDex().getAbsolutePath());
             String bootstrapClassNames = args.getBootstrapClassNames();
             if (bootstrapClassNames != null) {
-                extensionLoader.runBeforeApplicationCreateBootstrap(instance, bootstrapClassNames.split(","));
+                extensionLoader.runBeforeApplicationCreateBootstrap(this, bootstrapClassNames.split(","));
             }
         } else {
             extensionLoader = new ExtensionLoader(context);
@@ -104,92 +64,15 @@ public class LightweightInstrumentation extends Instrumentation implements Serve
                     extensionLoader.runAfterApplicationCreateBootstrap(instance, args.getBootstrapClassNames().split(","));
                 }
 
-                startMainActivity();
+                // Start the new activity
+                Intent intent = new Intent();
+                intent.setClassName(getTargetContext(), mainActivityName);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.setAction(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                getTargetContext().startActivity(intent);
             }
         });
-    }
-
-    public static synchronized LightweightInstrumentation getInstance() {
-        return instance;
-    }
-
-    @Override
-    public Application newApplication(ClassLoader cl, String className, Context context) throws InstantiationException,
-            IllegalAccessException, ClassNotFoundException {
-        UncaughtExceptionHandling.clearCrashLogFile();
-        UncaughtExceptionHandling.setGlobalExceptionHandler();
-        return super.newApplication(cl, className, context);
-    }
-
-    @Override
-    public void callActivityOnResume(Activity activity) {
-        super.callActivityOnResume(activity);
-
-        activitiesReporter.wasResumed(activity);
-    }
-
-    @Override
-    public void callActivityOnCreate(Activity activity, Bundle icicle) {
-        super.callActivityOnCreate(activity, icicle);
-
-        activitiesReporter.wasCreated(activity);
-    }
-
-    @Override
-    public void callActivityOnDestroy(Activity activity) {
-        activitiesReporter.wasDestroyed(activity);
-
-        super.callActivityOnDestroy(activity);
-    }
-
-
-    public Activity getCurrentActivity() {
-        return activitiesReporter.getCurrentActivity();
-    }
-
-    private Set<Activity> getActivities() {
-        return activitiesReporter.getActivities();
-    }
-
-    @Override
-    public void onDestroy() {
-        instance = null;
-    }
-
-    @Override
-    public String getServerVersion() {
-        Context context = getContext();
-        String versionName = "0.3";
-        try {
-            versionName =
-                    context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
-        } catch (NameNotFoundException e) {
-        }
-        return versionName;
-    }
-
-    @Override
-    public String getCpuArch() {
-        return android.os.Build.CPU_ABI;
-    }
-
-    @Override
-    public String getOsVersion() {
-        return String.valueOf(android.os.Build.VERSION.SDK_INT);
-    }
-
-    @Override
-    public JSONArray getSupportedApps() {
-        return new JSONArray();
-    }
-
-    @Override
-    public JSONArray getSupportedDevices() {
-        return new JSONArray();
-    }
-
-    @Override
-    public String getOsName() {
-        return "Android";
     }
 }
