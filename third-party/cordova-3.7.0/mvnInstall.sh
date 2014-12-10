8118f205128d1690557d58b7cd03db54d50a0a9a
Viacheslav Frolov <frolic@yandex-team.ru>
2014-12-10 22:39:12 +0300

    Triggering DOM event of type 'INPUT' after changing value of IPUT element. Unit tests.

--- third-party/cordova-3.7.0/mvnInstall.sh
+++ third-party/cordova-3.7.0/mvnInstall.sh
@@ -1,4 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
+pushd `dirname $0`
 mvn install:install-file -Dfile=classes.jar \
   -DgroupId=org.apache.cordova -DartifactId=cordova \
   -Dversion=3.7.0 -Dclassifier=android -Dpackaging=jar
+popd
