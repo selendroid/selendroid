e2aa11b14cb696b03230b0d3b229b506789023a0
Luke Petre <lpetre@fb.com>
2017-12-14 18:35:00 +0000

    If no activity is specified, do not start one

--- selendroid-server/src/main/java/io/selendroid/server/LightweightInstrumentation.java
+++ selendroid-server/src/main/java/io/selendroid/server/LightweightInstrumentation.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -66,10 +66,10 @@ public class LightweightInstrumentation extends Instrumentation {
                 } else {
                     // Start the new activity
                     String activity = args.getActivityClassName();
-                    Intent intent;
+                    Intent intent = null;
                     if (activity != null) {
                         intent = Intents.createStartActivityIntent(context, activity);
-                    } else {
+                    } else if (args.getIntentUri() != null) {
                         intent = Intents.createUriIntent(args.getIntentAction(), args.getIntentUri());
                     }
                     if (intent != null) {
@@ -81,4 +81,3 @@ public class LightweightInstrumentation extends Instrumentation {
         SelendroidLogger.info("LightweightInstrumentation initialized");
     }
 }
-
